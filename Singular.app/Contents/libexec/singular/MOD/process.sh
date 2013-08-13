481ed57ae4faff9674d4d4a31f0ae86af524d41e
Oleksandr Motsak <http://goo.gl/mcpzY>
2013-08-13 02:15:47 +0200

    More auotomation + later install_name_tool

--- Singular.app/Contents/libexec/singular/MOD/process.sh
+++ Singular.app/Contents/libexec/singular/MOD/process.sh
@@ -3,11 +3,23 @@
 # cat ./todo | xargs -I '{}' strip './{}' 
 
 
-#cat ./todo | xargs -I '{}' install_name_tool -change "/usr/lib/libedit.3.dylib" "@executable_path/../mylibs/libedit.dylib" "./{}" 
 #cat ./todo | xargs -I '{}' install_name_tool -change "/usr/lib/libncurses.5.4.dylib" "@executable_path/../mylibs/libncurses.dylib" "./{}"
 #cat ./todo | xargs -I '{}' install_name_tool -change "/usr/local/lib/libgmp.10.dylib" "@executable_path/../mylibs/libgmp.dylib" "./{}"
 
-cat ./todo | xargs -I '{}' otool -L './{}' 
+cat ./todo | xargs -I '{}' otool -L './{}' | sort | uniq | grep -E 'lib(gmp|ncurses|edit).*dylib' | sed -e 's| (.*$||g' -e 's|[ 	]*||g' > libs
+
+cat ./libs | sed -e 's|^.*/lib||g' | sed -e 's|\..*dylib$||g'  > names
+
+for n in `cat ./names` 
+do
+#  echo $n
+  L=`grep "${n}" ./libs`
+#  echo $L
+  cat ./todo | xargs -I '{}' ./install_name_tool -change "$L" "@executable_path/../mylibs/lib${n}.dylib" "./{}" 
+done
+
+#cat ./todo | xargs -I '{}' install_name_tool -change "/usr/lib/libedit.3.dylib" "@executable_path/../mylibs/libedit.dylib" "./{}" 
+
 # | sort | uniq
 
 exit 0
